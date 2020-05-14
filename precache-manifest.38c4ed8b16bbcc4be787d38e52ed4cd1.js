1166cf4b07a633e27dc1cfd6a4e9eb3410c94061
Charlotte Mountain <charr.mt@gmail.com>
2020-05-13 18:51:48 -0700

    Updates

---
+++ precache-manifest.38c4ed8b16bbcc4be787d38e52ed4cd1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94153b573563d33d822d25c623e3ca7c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ec8742fb89190cbb122a",
+    "url": "/portfolio/static/css/main.b0ed51c3.chunk.css"
+  },
+  {
+    "revision": "53fe3e1e2ba60ac9c841",
+    "url": "/portfolio/static/js/2.52c94536.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/portfolio/static/js/2.52c94536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8742fb89190cbb122a",
+    "url": "/portfolio/static/js/main.e2ddaf5b.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "074065d86018a05abc5624004e45d21b",
+    "url": "/portfolio/static/media/ADayOut.074065d8.gif"
+  },
+  {
+    "revision": "d49a4b72f8a94025793c0033897fbd5c",
+    "url": "/portfolio/static/media/Burger.d49a4b72.gif"
+  },
+  {
+    "revision": "2aa804db5c9f10cd473a5cefd66853da",
+    "url": "/portfolio/static/media/burger.2aa804db.jpg"
+  },
+  {
+    "revision": "064adeba662ac9afd4408cbec8a8a79b",
+    "url": "/portfolio/static/media/charlotte.064adeba.jpg"
+  },
+  {
+    "revision": "80d9f5d2a120acaa902af53fbd9bc609",
+    "url": "/portfolio/static/media/met.80d9f5d2.jpg"
+  },
+  {
+    "revision": "2ae4c8373eb034674520814225f6babc",
+    "url": "/portfolio/static/media/mountain.2ae4c837.jpg"
+  },
+  {
+    "revision": "134533f048bc2adbc8f60435bcc6bcf2",
+    "url": "/portfolio/static/media/note.134533f0.jpg"
+  },
+  {
+    "revision": "e6250edb3575a4448b399627b767fdf0",
+    "url": "/portfolio/static/media/noteDemo.e6250edb.gif"
+  },
+  {
+    "revision": "0e60ff9752ec44709cd19c2df84bbddc",
+    "url": "/portfolio/static/media/sat.0e60ff97.jpg"
+  },
+  {
+    "revision": "191dbfec1fde479d1ef1fa69ed5ec4f4",
+    "url": "/portfolio/static/media/weather.191dbfec.gif"
+  },
+  {
+    "revision": "d323c94879532e35fe01d85b34116932",
+    "url": "/portfolio/static/media/weather.d323c948.jpg"
+  },
+  {
+    "revision": "c6f3517eaee61ca4f2dbc5f1bed1369c",
+    "url": "/portfolio/static/media/whatsup.c6f3517e.gif"
+  },
+  {
+    "revision": "07c9a5cad79142fdc47f81fdc0a0a379",
+    "url": "/portfolio/static/media/work.07c9a5ca.jpg"
+  },
+  {
+    "revision": "1a3a635ebceee80b0179a233cac99d7c",
+    "url": "/portfolio/static/media/work.1a3a635e.gif"
+  }
+]);
